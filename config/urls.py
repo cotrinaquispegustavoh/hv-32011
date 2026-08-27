02bf5084686da24f45a6d5c9f3d77d1382d181a7
GusTavo CQ <cotrinaquispe.gustavoh@gmail.com>
2026-08-26 21:45:45 -0500

    correcion en despliegue

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('django-admin/', admin.site.urls),
@@ -14,6 +15,13 @@ urlpatterns = [
     path('asignaciones/', include('apps.assignments.urls')),
     path('academico/', include('apps.academics.urls')),
     path('', include('apps.core.urls')),
+    
+    # --- PARCHE PARA SERVIDOR DE PRUEBAS (RENDER) ---
+    # Forzamos a Django a mostrar las imágenes subidas por los usuarios 
+    # incluso cuando DEBUG=False (Producción).
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 ]
 
 if settings.DEBUG:
